aefbf15b17cf86656d12c7ee1336f069ccb6386d
onetayjones <72883059+onetayjones@users.noreply.github.com>
2022-07-06 18:18:54 +0200

    Creating  Login Forms
    Different Login forms for members/librarians

--- src/inc/login.inc.php
+++ src/inc/login.inc.php
@@ -34,7 +34,20 @@ session_start();
 <div class="row">
   <div class="col-md-6 mx-auto">
     <div class="card card-body bg-light mt-5">
-      <h2>Login</h2>
+      <div class="row">
+        <div class="col member">
+          <h2 class="login-text"> Member Login</h2>
+        </div>
+
+        <div class="col librarian">
+          <h2 class="login-text">Librarian Login</h2>
+        </div>
+      </div>
+
+
+
+
+      <!-- <h2>Login</h2> -->
       <p>Please fill in your credientails</p>
       <form class="" action="login.php" method="post">
 
@@ -58,7 +71,7 @@ session_start();
         </div>
         <div class="row">
           <div class="col">
-            <input type="submit" name="login" value="Login" class="btn btn-success btn-block mt-2">
+            <input type="submit" name="login" value="Login" class="btn btn-success btn-block shadow w-50 p-2 rounded mt-2">
           </div>
           <div class="col">
             <a href="register.php" class="btn btn-light btn-block">No account? Sign up today</a>
